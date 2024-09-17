d2569e5f584859cfc9e6537213e9116667e46b87
rae-afghani <razankafghani@gmail.com>
2024-09-16 20:02:18 -0500

    Set up WeatherAPI and created methods to handle search functions, also imported icons from Material

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -1,12 +1,69 @@
 import { Component } from '@angular/core';
+import { WeatherApiService } from '../../services/weather-api.service';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule, MatIconButton } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { SearchbarComponent } from '../../ui/searchbar/searchbar.component';
+import { Weather } from '../../models/weather.model';
+import { MatIcon } from '@angular/material/icon';
+import {MatRadioModule} from '@angular/material/radio';
 
 @Component({
   selector: 'app-weather',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    SearchbarComponent,
+    MatTabsModule,
+    ReactiveFormsModule,
+    MatSlideToggleModule,
+    MatIconButton,
+    MatIcon,
+    MatRadioModule
+  ],
   templateUrl: './weather.component.html',
-  styleUrl: './weather.component.scss'
+  styleUrl: './weather.component.scss',
 })
 export class WeatherComponent {
+  weatherData: Weather | null = null;
+  errorMessage: string | null = null;
+  toggleForm: FormGroup;
+  unitControl: FormControl;
+  location: string = 'Aveiro';
 
+  constructor(private weatherService: WeatherApiService, private formBuilder: FormBuilder) {
+    this.unitControl = new FormControl(1); // 1 - Celsius  2 - Fahrenheit
+    this.toggleForm = this.formBuilder.group({
+      unit: this.unitControl
+    });
+  }
+
+  handleSearch(searchText: string) {
+    this.location = searchText;
+    this.errorMessage = null;
+    this.weatherService.getWeather(this.location).subscribe(
+      (data) => {
+        this.weatherData = data;
+        console.log(this.weatherData);
+      },
+      (error) => {
+        this.weatherData = null;
+        this.errorMessage = 'Please check your location name and try again.';
+      }
+    );
+  }
+
+  getTemperature(tempC: number, tempF: number): number {
+    return this.unitControl.value === 1 ? tempC : tempF;
+  }
 }
--- src/app/services/weather-api.service.ts
+++ src/app/services/weather-api.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Weather } from '../models/weather.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherApiService {
+  private apiKey = '0aea6d983ec3411f9f2231237241609';
+  private apiUrl = 'http://api.weatherapi.com/v1';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  
+  // getWeather(location: string): Observable<Weather> {
+  //   return this.http.get<any>(`${this.apiUrl}/forecast.json?key=${this.apiKey}&q=${location}&days=7&aqi=no`);
+  // }
+
+  getWeather(location: string, days: number = 7): Observable<any> {
+    const params = new HttpParams()
+      .set('key', this.apiKey)
+      .set('q', location)
+      .set('days', days.toString());
+
+      console.log(`${this.apiUrl}/forecast.json`, { params });
+    return this.http.get(`${this.apiUrl}/forecast.json`, { params });
+  }
 }
